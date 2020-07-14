c81965e353a785a0bcfe9900fa0defdbaeb439f6
Szczepanski-Marcin <szmar@vp.pl>
2020-07-14 19:44:00 +0200

    Added a function to chose a color of marker

--- map.py
+++ map.py
@@ -6,13 +6,21 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
+def color_producer(elevation):
+    if elevation <1800:
+        return "green"
+    elif 1800 <= elevation <2500:
+        return "orange"
+    else:
+        return "red"
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, titles="Stamen Terrian")
 
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" m", icon=folium.Icon(color='green')))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius='7', popup=str(el)+" m", fill_color=color_producer(el,), color='grey', fill_opacity=0.7))
+
 
 map.add_child(fg)
-
 map.save("Map1.html")
